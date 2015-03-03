a806694ec617468886b9f7b1f5cb0896862d72ff
Rayanb <rayanbouts@gmail.com>
2015-03-03 18:51:15 -0500

    ajax get and post

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,3 +3,12 @@ get '/' do
   erb :index
 end
 
+post '/header' do
+  message = params[:message]
+  content_type :json
+  {message: message}.to_json
+end
+
+get '/lightsaber' do
+  erb :lightsaber, layout: false
+end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,26 @@
 // Javascript Goes Here~
 //
-function Calculator(){
-  return {
-
-  }
-
-};
+$(document).on('ready', function() {
+  $('.lightsaber').css("display", "none");
+  $('.amaze').on('click', function(event){
+    event.preventDefault();
+    $.get('/lightsaber', function(response){
+      $('.lightsaber').append(response);
+      $('.lightsaber').toggle("slow");
+      var text = $('.amaze').text();
+      $('.amaze').text(text == "amazed" ? "deceived" : "amazed");
+    })
+  })
+  $('.header').on('submit', function(event){
+    event.preventDefault();
+    var $target = $(event.target);
+    $.ajax({
+      url:'/header',
+      type: 'post',
+      data: $target.serialize(),
+      success: function(response){
+        $('.message').text(response.message+"'s")
+      }
+    })
+  })
+})
